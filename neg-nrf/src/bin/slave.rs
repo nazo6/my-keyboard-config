01efb65df7dc9c07fed2ba591c3794c0f6257fa8
nazo6 <git@nazo6.dev>
2025-11-02 21:52:10 +0900

    fix

--- neg-nrf/src/bin/slave.rs
+++ neg-nrf/src/bin/slave.rs
@@ -3,10 +3,12 @@
 #![feature(impl_trait_in_assoc_type)]
 
 use embassy_executor::Spawner;
-use neg_nrf::start_slave;
+use neg_nrf::{init_peri, start_slave};
 use rktk::hooks::create_empty_hooks;
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    start_slave(create_empty_hooks()).await;
+async fn main(spawner: Spawner) {
+    let p = init_peri();
+
+    start_slave(spawner, p, create_empty_hooks()).await;
 }
